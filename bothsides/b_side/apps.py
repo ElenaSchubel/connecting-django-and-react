81592b4032f2e5230dc0561e8734076bbc25cbc9
elena <ebassist@live.com>
2019-03-09 22:04:23 +1300

    added react for the frontend

--- bothsides/b_side/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BSideConfig(AppConfig):
-    name = 'b_side'
---
+++ bothsides/customers/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Customers(AppConfig):
+    name = 'customers'
